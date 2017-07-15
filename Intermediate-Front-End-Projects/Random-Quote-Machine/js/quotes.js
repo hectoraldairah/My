ef494ddee2d6e5edc361a4c3ef54291d6f690a72
Héctor Aldair Aguilar Hernández <hectoraldairah@gmail.com>
2017-07-14 20:54:09 -0500

    Fix Tweet button

--- Intermediate-Front-End-Projects/Random-Quote-Machine/js/quotes.js
+++ Intermediate-Front-End-Projects/Random-Quote-Machine/js/quotes.js
@@ -1,3 +1,42 @@
-$(document).ready(function(){
-    $('.container').addClass('animated fadeIn');
+$(document).ready(function () {
+    $('#title').addClass('animated fadeIn');
+    $('#quote-card').addClass('animated zoomIn');
+    var theQuote = '';
+    var theAuthor = '';
+   newQuote();
+
+    $('#button-1').on("click", function () {
+        newQuote();
+    });
+
+    $('#button-2').on("click", function () {
+        var twitterUrl = 'https://twitter.com/intent/tweet?text=' + theQuote + ' ' + theAuthor;
+        window.open(twitterUrl, 'twitter');
+        return false;
+    });
+
+    function newQuote()
+    {
+        var quotes = { 
+          1: [' You know the story about the scorpion and the frog? Your friend Nino didn\'t make it across the river.', 'Drive'],
+          2: [' This is from... Mathilda.', 'Leon the Professional'],
+          3: ['The first rule of Fight Club is: You do not talk about Fight Club.', 'Fight Club'],
+          4: ['You talkin \' to me?.', 'Taxi Driver'],
+          5: [],
+          6: [],
+          7: [],
+          8: [],
+          9: [],
+          19: []
+
+        };
+
+        var randomNumber = Math.ceil(Math.random() * Object.keys(quotes).length);
+
+         var theQuote = $('#quote').text(quotes[randomNumber][0]);
+         var theAuthor = $('#author').text(quotes[randomNumber][1]);
+       
+       
+    }
+        
 });
