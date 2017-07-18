396e20490ebfa0b529965b283ea2c0ec9ff160a2
Héctor Aldair Aguilar Hernández <hectoraldairah@gmail.com>
2017-07-18 02:46:36 -0500

    API works

--- Intermediate-Front-End-Projects/Show-the-Local-Weather/js/weather.js
+++ Intermediate-Front-End-Projects/Show-the-Local-Weather/js/weather.js
@@ -2,24 +2,39 @@ $(document).ready(function(){
     var userPosition = 
     {
         lat: '',
-        lon: ''
+        long: ''
     };
+    var urlAPI = 'https://api.darksky.net/forecast/409d2ec2e2ba317c8037fc0eba1d97bd/';
+    getPosition();
 
-    var key = '409d2ec2e2ba317c8037fc0eba1d97bd';
+    function getPosition() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                userPosition.lat = position.coords.latitude;
+                userPosition.long = position.coords.longitude;
+                console.log(userPosition.lat); //This shows me the latitude
+                showPosition(userPosition.lat,userPosition.long);
+            });
+        }
+        else {
+            alert("Geolocation not supported by your browser");
+        }
 
-        if(navigator.geolocation)
-            {
-                navigator.geolocation.getCurrentPosition(function(position){
-                    userPosition.lat = position.coords.latitude;
-                    userPosition.lon = position.coords.longitude;
-                    console.log(userPosition.lat); //This shows me the latitude
-                });
-            }
-            else
-            {
-                alert("Geolocation not supported by your browser");
-            }
-    
-    console.log(userPosition.lat);      // This not works
+    }
 
+    function showPosition(lat,long)
+    {
+        urlAPI = urlAPI + lat + ',' + long;
+        getJSON(urlAPI);
+    }
+
+    function getJSON(urlAPI)
+    {
+        $.ajax({
+            dataType: "jsonp",
+            url : urlAPI ,}).done(function(data){
+                alert(data.currently.te);
+            });
+        
+    }
 });
