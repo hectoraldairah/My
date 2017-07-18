f503557fb194a437505b9b7da07e79e138c9e019
Héctor Aldair Aguilar Hernández <hectoraldairah@gmail.com>
2017-07-17 23:14:36 -0500

    Fix latitude and longitude

--- Intermediate-Front-End-Projects/Show-the-Local-Weather/js/weather.js
+++ Intermediate-Front-End-Projects/Show-the-Local-Weather/js/weather.js
@@ -1,1 +1,28 @@
+$(document).ready(function(){
+    var userPosition = 
+    {
+        lat: '',
+        lon: ''
+    };
 
+    getLocation();
+
+    function getLocation()
+    {
+        if(navigator.geolocation)
+            {
+                navigator.geolocation.getCurrentPosition(function(position){
+                    userPosition.lat = position.coords.latitude;
+                    userPosition.lon = position.coords.longitude;
+                    console.log(userPosition.lat);
+                });
+            }
+            else
+            {
+                alert("Geolocation not supported by your browser");
+            }
+    }
+
+    console.log(userPosition.lat);
+  
+});
