38af762aad087404196cac08596d35a6e826fd08
Héctor Aldair Aguilar Hernández <hectoraldairah@gmail.com>
2017-07-20 02:20:57 -0500

    Time add

--- Intermediate-Front-End-Projects/Show-the-Local-Weather/js/weather.js
+++ Intermediate-Front-End-Projects/Show-the-Local-Weather/js/weather.js
@@ -33,17 +33,52 @@ $(document).ready(function(){
         $.ajax({
             dataType: "jsonp",
             url : urlAPI ,}).done(function(data){
+           //Center Card//
             $('#temperature').html(Math.round(data.currently.temperature));
             
              $('#celcious').click(function(){
-                 $('#temperature').html(Math.round((data.currently.temperature * 5) / 9));
+                 $('#temperature').html(Math.round((data.currently.temperature - 32) * (5 / 9)));
+                  $(this).toggleClass('clicked');
+                  $('#farenheit').removeClass('clicked');
              });
 
               $('#farenheit').click(function(){
                  $('#temperature').html(Math.round(data.currently.temperature));
+                  $(this).toggleClass('clicked');
+                  $('#celcious').removeClass('clicked');
+
              });
+            
+            //Left Card//
+
+            $('#summary').html(data.currently.summary);
+             
+            var iconSelect = $('#icon-summary').html(data.currently.icon).text();
+            
+            switch(iconSelect) {
+                case 'clear-day': $('#icon-summary').html('<i class = "wi wi-day-sunny"></i>');break;
+                case 'rain': $('#icon-summary').html('<i class = "wi wi-rain"></i>');break;
+                case 'snow': $('#icon-summary').html('<i class = "wi wi-snow"></i>');break;
+                case 'sleet': $('#icon-summary').html('<i class = "wi wi-sleet"></i>');break;
+                case 'wind': $('#icon-summary').html('<i class = "wi wi-windy"></i>');break;
+                case 'fog': $('#icon-summary').html('<i class = "wi wi-fog"></i>');break;
+                case 'fog': $('#icon-summary').html('<i class = "wi wi-fog"></i>');break;
+                case 'cloudy': $('#icon-summary').html('<i class = "wi wi-cloudy"></i>');break;
+                case 'partly-cloudy-day': $('#icon-summary').html('<i class = "wi wi-day-cloudy"></i>');break;
+                case 'partly-cloudy-night': $('#icon-summary').html('<i class = "wi wi-night-cloudy "></i>');break;
+            }
+               
+
+             
+             
+            
+
+
 
             });
         
     }
+           $("#farenheit").toggleClass('clicked');
+            $('#quote-card').addClass('animated fadeInUp');
+     
 });
